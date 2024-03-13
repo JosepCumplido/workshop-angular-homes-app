cca54911bfea806a231c32464054382d1330dfb7
Bernat <girobernat@gmail.com>
2024-03-13 20:06:46 +0100

    Starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
@@ -7,15 +6,14 @@ import { HomeComponent } from './home/home.component';
   template: `
     <main>
       <header class="brand-name">
-        <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        <app-home></app-home>
+        <b>Hello World!</b>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent],
+  imports: [],
 })
 export class AppComponent {
   title = 'homes';
